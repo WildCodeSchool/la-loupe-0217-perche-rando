f8e3fcccb96b1f9f31b56d548070eedf6527b6b5
thmeme <thierrymeme@gmail.com>
2017-03-28 14:17:13 +0200

    météo 5 days ok

--- app/js/controllers/trailController.js
+++ app/js/controllers/trailController.js
@@ -2,28 +2,44 @@
  * Correspond à l'affichage d'un circuit
  */
 angular.module('app')
-    .controller('TrailController', function($scope, $stateParams, TrailService, NgMap) {
+    .controller('TrailController', function($scope, $stateParams, TrailService, NgMap, WeatherService) {
         $scope.idTrail = $stateParams.id;
         $scope.error = {
             'content': '',
             'show': false
         };
 
-        TrailService.getOne($scope.idTrail).then(function(res) {
-            console.log(res);
-            $scope.trail = res.data;
-            console.log($scope.trail);
-            NgMap.getMap().then(function(map) {
-                console.log(map);
+        // TrailService.getOne($scope.idTrail).then(function(res) {
+        //     console.log(res);
+        //     $scope.trail = res.data;
+        //     console.log($scope.trail);
+        //     NgMap.getMap().then(function(map) {
+        //         console.log(map);
+        //
+        //         map.setCenter($scope.trail.center);
+        //         map.setZoom($scope.trail.zoom);
+        //     });
+        //
+        // }, function(err) {
+        //     $scope.error.show = true;
+        //     $scope.error.content = err.statusText;
+        //     console.log(err);
+        // });
 
-                map.setCenter($scope.trail.center);
-                map.setZoom($scope.trail.zoom);
-            });
+        $scope.Ville = 'Chartres';
+        WeatherService.getWeather($scope.Ville).then(function(res) {
+            console.log(res);
+            $scope.weather = res.data.list;
+            console.log($scope.weather);
+
         }, function(err) {
             $scope.error.show = true;
             $scope.error.content = err.statusText;
-            console.log(err);
+            console.log('erreur', err);
         });
-
+        $scope.changeDate = function(date) {
+          date = date.substring(0,10).split("-").reverse().join("-");
+          return date;
+        };
 
     });
--- app/js/services/weatherService.js
+++ app/js/services/weatherService.js
@@ -4,5 +4,9 @@
 angular.module('app')
     .service('WeatherService', function($http) {
         return {
+          getWeather: function (City) {
+            console.log(`http://api.openweathermap.org/data/2.5/forecast?q=${City}&lang=fr&units=metric&APPID=a59c2729aa50f4e50a18170f3ac6814d`);
+            return $http.get(`http://api.openweathermap.org/data/2.5/forecast?q=${City}&lang=fr&units=metric&APPID=a59c2729aa50f4e50a18170f3ac6814d`);
+          }
         };
     });
