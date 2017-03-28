aa23a14783c2e84b91ed492dfcc65d8090998492
thmeme <thierrymeme@gmail.com>
2017-03-28 08:41:57 +0200

    buttons ok 3/3

--- app/js/controllers/listController.js
+++ app/js/controllers/listController.js
@@ -7,17 +7,22 @@ angular.module('app')
         TrailService.getList().then(function(res) {
             $scope.Cities = res.data;
             console.log($scope.Cities);
+            console.log(res);
           });
 
         $scope.applyFilters = function() {
             console.log('distance', $scope.filterDistance);
             console.log('city', $scope.filterCity);
             console.log('stars', $scope.filterRating);
+
+            let id = $scope.filterCity;
+            $scope.filters = [];
+            console.log(id);
+            $scope.filter.push($scope.cities);
+
         };
 
-        TrailService.getList().then(function(res) {
-            console.log(res);
 
-        });
+
 
     });
