d0acefbf1a19320be340b00678cfcbbd477cbd0b
maallard <mar.allard@gmail.com>
2017-04-06 21:39:31 +0200

    Added deletion of trails by the author

--- server/api/middlewares/authorization.js
+++ server/api/middlewares/authorization.js
@@ -1,5 +1,6 @@
 import jsonwebtoken from 'jsonwebtoken';
 import token from '../token.js';
+import Trail from '../models/trail.js';
 
 export default {
 
@@ -22,6 +23,30 @@ export default {
             jsonwebtoken.verify(req.headers.authorization, token, (err, decoded) => {
                 if (decoded._doc && decoded._doc.isAdmin) {
                     next();
+                } else {
+                    return res.sendStatus(403);
+                }
+            });
+        } else {
+            return res.sendStatus(401);
+        }
+    },
+
+    isAuthor: (req, res, next) => {
+        if (req.headers.authorization) {
+            jsonwebtoken.verify(req.headers.authorization, token, (err, decoded) => {
+                if (decoded._doc) {
+                    let trail = new Trail();
+                    trail.findByIdForAuth(req.params.id, (err, trail) => {
+                        if (trail.author._id.toString() === decoded._doc._id) {
+                            next();
+                        } else {
+                            return res.status(403).json({
+                                success: false,
+                                message: "You are not the author of this trail"
+                            });
+                        }
+                    });
                 } else {
                     return res.sendStatus(403);
                 }
