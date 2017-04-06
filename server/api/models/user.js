0185a7175b4546291dd85d2207f4c482de0abd00
thmeme <thierrymeme@gmail.com>
2017-04-06 16:53:17 +0200

    cript password update ok

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -133,9 +133,13 @@ export default class User {
     }
 
     update(req, res) {
+      if (req.body.password) {
+          var salt = bcrypt.genSaltSync(10);
+          req.body.password = bcrypt.hashSync(req.body.password, salt);
+      }
         model.update({
             _id: req.params.id,
-            
+
         }, req.body, (err, user) => {
             if (err || !user) {
                 res.status(500).send(err.message);
